61a9ddc00a5da3d5f36d87d00333f373c3d2b7b4
SGNayak12 <GNayakSuhas12@gmail.com>
2025-11-07 10:16:44 +0530

    updated index.js

--- index.js
+++ index.js
@@ -21,6 +21,7 @@ const storage = multer.diskStorage({
 
 // multer configuration
 const upload = multer({storage: storage})
+
 app.use(
   cors({
     origin: ["http://localhost:8000", "http://localhost:5173"],
@@ -42,13 +43,13 @@ app.use(express.urlencoded({extended: true}))
 app.use("/uploads", express.static("uploads"))
 
 app.get('/', function(req, res){
-  res.json({message: "Hello chai aur code"})
+  res.json({message: "Hi there"})
 })
 
 app.post("/upload", upload.single('file'), function(req, res){
-  const lessonId = uuidv4()
+  const id = uuidv4()
   const videoPath = req.file.path
-  const outputPath = `./uploads/courses/${lessonId}`
+  const outputPath = `./uploads/vedios/${id}`
   const hlsPath = `${outputPath}/index.m3u8`
   console.log("hlsPath", hlsPath)
 
